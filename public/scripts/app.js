b471fffc25bfa0815dcf5731ffbd58afc9a6b73f
alex <you@example.com>
2019-04-16 22:17:56 +0000

    added rendertweets functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,33 +3,74 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
 function createTweetElement (tweet) {
-     
-return `<article class="tweet"><header><img "src="${tweet.user.avatars.small}" alt="Avatar" class="avatar"></img><h3>${tweet.user.name}</h3></header><p class="body-text"> ${tweet.content.text} </p><footer><p class="footer-text">${tweet.created_at}</p></footer></article>`
-     
+   return `<article class="tweet">
+        <header>
+            <img src="${tweet.user.avatars.small}" alt="Avatar" class="avatar"/>
+            <h3>${tweet.user.name}</h3>
+        </header>
+        <p class="body-text">${tweet.content.text}</p>
+        <footer>
+            <p class="footer-text">${tweet.created_at}</p>
+        </footer>
+    </article>`;
 }
 
-const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
-  
-  var $tweet = createTweetElement(tweetData);
-  
-  console.log($tweet); // to see what it looks like
-  $('.tweet-container').append($tweet);
+function renderTweets(tweets) {
+    for (let tweet of tweets) {
+        $('.tweet-container').append(createTweetElement(tweet));
+    }
+}
 
+$(document).ready(function() {
+    renderTweets(data);
 });
+
